f49815d0937c9081ec4d00fed37b2ab4ae912752
xpieemma <7chrispeter@gmail.com>
2026-01-12 23:28:48 -0500

    index.html connected to logic.js and checked addTask, updateStatus, deleteTask functions

--- logic.js
+++ logic.js
@@ -16,3 +16,46 @@ const addTask = (tasks, name, category, deadline) => {
   newTasks.push(newTask);
   return newTasks;
 };
+
+const deleteTask = (tasks, taskId) => {
+  const newTasks = [];
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].id !== taskId) {
+      newTasks.push(tasks[i]);
+    }
+  }
+  return newTasks;
+};
+
+const updateStatus = (tasks, taskId, newStatus) => {
+const newTasks = [];
+for (let i = 0; i < tasks.length; i++){
+    if (tasks[i].id === taskId){
+        const updatedTask ={
+            id : tasks[i].id,
+            name: tasks[i].name,
+            category: tasks[i].category,
+            deadline: tasks[i].deadline,
+            status: newStatus
+        };
+        newTasks.push(updatedTask);
+    } else {
+        newTasks.push(tasks[i]);
+    }
+}
+return newTasks;
+};
+
+let state = [];
+state = addTask(state, "Buy Milk", "Groceries", "2024-01-20");
+state = addTask(state, "Clean Room", "Chores", "2024-01-21");
+console.log("Add Tasks Result:", state);
+
+// Update Status (using the ID of the first task)
+const firstId = state[0].id;
+state = updateStatus(state, firstId, "Completed");
+console.log("2. Update Status Result:", state);
+
+// Delete Task
+state = deleteTask(state, firstId);
+console.log("Delete Task Result (Should be 1 left):", state);
